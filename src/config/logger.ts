91c2e1354861684cbf17e1b10a9485013e7fc6bb
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-11-24 01:54:28 +0530

    add: logger setup

---
+++ src/config/logger.ts
@@ -0,0 +1,26 @@
+import winston from "winston";
+import { Config } from "./index";
+
+const logger = winston.createLogger({
+    level: "info",
+    format: winston.format.json(),
+    defaultMeta: { serviceName: "auth-service" },
+    transports: [
+        new winston.transports.File({
+            level: "info",
+            dirname: "logs",
+            filename: "combined.log",
+            silent: Config.NODE_ENV === "test",
+        }),
+        new winston.transports.Console({
+            level: "info",
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json(),
+            ),
+            silent: Config.NODE_ENV === "test",
+        }),
+    ],
+});
+
+export default logger;
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,21 @@
 import app from "./app";
 import { Config } from "./config";
+import logger from "./config/logger";
 
 const startServer = () => {
     const PORT = Config.PORT;
     try {
         app.listen(PORT, () =>
-            // eslint-disable-next-line no-console
-            console.log(`Server linstening on port ${PORT}`),
+            logger.info(`Server linstening on port ${PORT}`),
         );
     } catch (error) {
         // eslint-disable-next-line no-console
-        console.log(error);
-        process.exit(1);
+        if (error instanceof Error) {
+            logger.error(error.message);
+        }
+        setTimeout(() => {
+            process.exit(1);
+        }, 1000);
     }
 };
 
