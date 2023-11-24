76074c365fa39ef307d85ca44b8b0d42d5adf507
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-11-24 13:23:58 +0530

    add: register endpoint test

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,6 +1,6 @@
 import { Response, Request } from "express";
 export class AuthController {
     register(req: Request, res: Response) {
-        res.status(201).send();
+        res.status(201).json();
     }
 }
--- tests/user/register.test.ts
+++ tests/user/register.test.ts
@@ -3,20 +3,29 @@ import app from "../../src/app";
 
 describe("POST /api/auth/register", () => {
     describe("Given all fields", () => {
-        it("should returns the 201 status code", async () => {
-            // AAA Principle
-            const userData = {
-                fullName: "Rushikesh Mungse",
-                email: "mungse.rushi@gmail.com",
-                password: "secret",
-                confirmPassword: "secret",
-            };
+        const userData = {
+            fullName: "Rushikesh Mungse",
+            email: "mungse.rushi@gmail.com",
+            password: "secret",
+            confirmPassword: "secret",
+        };
 
+        it("should returns the 201 status code", async () => {
             const response = await request(app)
                 .post("/api/auth/register")
                 .send(userData);
 
             expect(response.statusCode).toBe(201);
+        });
+
+        it("should returns json response", async () => {
+            const response = await request(app)
+                .post("/api/auth/register")
+                .send(userData);
+
+            expect(
+                (response.headers as Record<string, string>)["content-type"],
+            ).toEqual(expect.stringContaining("json"));
         });
     });
 
