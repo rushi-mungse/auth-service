acbdb647bcf7b1abfefbf71f56189c9185f0d252
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-11-27 18:30:35 +0530

    add: isJWT method

--- tests/utils/index.ts
+++ tests/utils/index.ts
@@ -18,3 +18,20 @@ export const isHashOtp = (hashOtp: string): boolean => {
     if (parts.length !== 3) return false;
     return true;
 };
+
+// to check jwt token is valid
+export const isJWT = (token: string | null): boolean => {
+    if (token === null) return false;
+    const parts = token.split(";");
+
+    if (parts.length !== 3) return false;
+
+    try {
+        parts.forEach((part) => {
+            Buffer.from(part, "base64").toString("utf-8");
+        });
+        return true;
+    } catch (error) {
+        return false;
+    }
+};
