5ad4835ec1025f1756528eb38fb6eb3a2bfba4e2
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-11-27 21:58:57 +0530

    add: generation secret keys

---
+++ scripts/generateKeys.mjs
@@ -0,0 +1,17 @@
+import crypto from "crypto";
+import fs from "fs";
+
+const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
+    modulusLength: 2048,
+    publicKeyEncoding: {
+        type: "pkcs1",
+        format: "pem",
+    },
+    privateKeyEncoding: {
+        type: "pkcs1",
+        format: "pem",
+    },
+});
+
+fs.writeFileSync("/certs/private.pem", privateKey);
+fs.writeFileSync("/certs/public.pem", publicKey);
