503b15e188b63535e08c386db335a0ea455a46d1
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-11-27 12:27:54 +0530

    add: VerifyOtpRequest and VerifyOtpData type

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,6 +7,17 @@ export interface UserData {
     confirmPassword?: string;
     role?: string;
 }
-export interface RegisterUserRequest extends Request {
+export interface SendOtpRequest extends Request {
     body: UserData;
+}
+
+export interface VerifyOtpData {
+    fullName: string;
+    email: string;
+    hashOtp: string;
+    otp: string;
+}
+
+export interface VerifyOtpRequest extends Request {
+    body: VerifyOtpData;
 }
