3e705acc86cda44651d197c081fff9922a9b8c81
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-11-28 14:18:36 +0530

    add: cookie-parser and static middleware

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,12 @@ import "reflect-metadata";
 import express from "express";
 import { errorHandler } from "./middlewares";
 import authRouter from "./routes/auth";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.use(cookieParser());
+app.use(express.static("public"));
 app.use(express.json());
 
 // register auth router
