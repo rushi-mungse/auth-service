bd5db3a1c4bd5317a8977e51d3fff55376ea7968
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-12-02 00:23:55 +0530

    add: refactor access token middleware

--- src/middlewares/accessTokenMiddleware.ts
+++ src/middlewares/accessTokenMiddleware.ts
@@ -13,14 +13,12 @@ export default expressjwt({
     algorithms: ["RS256"],
     getToken(req: Request) {
         const authHeader = req.headers.authorization;
-
         if (authHeader && authHeader.split(" ")[1] !== "undefined") {
             const accessToken = authHeader.split(" ")[1];
             if (accessToken) {
                 return accessToken;
             }
         }
-
         const { accessToken } = req.cookies as AuthCookie;
         return accessToken;
     },
