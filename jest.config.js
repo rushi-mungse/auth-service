ac7178fc8601debd98e01d0373ae0519432a87f0
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-12-02 10:23:32 +0530

    add: coverage conf

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,7 @@ module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
     verbose: true,
+    collectCoverage: true,
+    coverageProvider: "v8",
+    collectCoverageFrom: ["src/**/*.ts", "!tests/**", "!**/node_modules/**"],
 };
