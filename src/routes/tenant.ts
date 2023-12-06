82c5814328cdd23c1581e304b0fc254fee7785ed
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-12-07 00:26:23 +0530

    add: tenant routes

--- src/routes/tenant.ts
+++ src/routes/tenant.ts
@@ -1,8 +1,36 @@
-import express from "express";
+import express, {
+    NextFunction,
+    Request,
+    RequestHandler,
+    Response,
+} from "express";
+import { TenantController } from "../controllers";
+import { TenantRequest } from "../types";
+import { TenantService } from "../services";
+import { AppDataSource } from "../config";
+import { Tenant } from "../entity";
+import { createTenantValidator } from "../validators";
+import { accessTokenMiddleware, canAccess } from "../middlewares";
+import { Role } from "../constants";
 const router = express.Router();
 
-router.post("/create", (req, res) => {
-    res.json({});
-});
+const tenantRepository = AppDataSource.getRepository(Tenant);
+const tenantService = new TenantService(tenantRepository);
+const tenantController = new TenantController(tenantService);
+
+router.post(
+    "/create",
+    [
+        createTenantValidator as unknown as RequestHandler,
+        accessTokenMiddleware,
+        canAccess([Role.ADMIN]),
+    ],
+    (req: Request, res: Response, next: NextFunction) =>
+        tenantController.create(
+            req as TenantRequest,
+            res,
+            next,
+        ) as unknown as RequestHandler,
+);
 
 export default router;
